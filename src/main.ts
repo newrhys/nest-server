30457e1495da3e933bb70414c7090f9e39ffaec7
rhys <onlyrhys@outlook.com>
2021-11-29 22:37:40 +0800

    feat: initialize swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { LoggerService } from './modules/logger/logger.service'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ConfigService } from '@nestjs/config'
+
+const logger = new LoggerService()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   // 注入日志
   const loggerService = app.get(LoggerService)
-  // console.log(loggerService)
   app.useLogger(loggerService)
+  // 获取配置服务
+  const configService = app.get<ConfigService>(ConfigService)
+  // swagger 接口文档
+  const swagger = configService.get('swagger')
+  const serve = configService.get('serve')
+  app.setGlobalPrefix(serve.prefix)
+  logger.info(serve, 'swagger configuration')
+  const documentBuilder = new DocumentBuilder()
+    .setTitle(swagger.title)
+    .setDescription(swagger.description)
+    .addBearerAuth()
+    .addServer(serve.prefix)
+    .build()
+  const document = SwaggerModule.createDocument(app, documentBuilder, {
+    ignoreGlobalPrefix: true
+  })
+  SwaggerModule.setup(swagger.path, app, document)
   await app.listen(3000)
 }
 
-bootstrap()
+bootstrap().then(() => {
+  logger.info('3000', '启动成功')
+})
